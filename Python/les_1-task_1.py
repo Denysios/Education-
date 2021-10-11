554e320c39fe4bb40e7e2028870aa6bbaf5f1d69
Denysios <denis.ugnivenko@outlook.com>
2021-10-11 11:59:12 +0300

    Merge remote-tracking branch 'origin/master'

--- Python/les_1-task_1.py
+++
@@ -1,6 +0,0 @@
-# Запрос числа у пользователя
-number = int(input('Enter number'))
-# Прибавление к этому числу цифру 2
-result = number + 2
-# Вывод результата на экран
-print (result)
--- Python/les_2-task_1.py
+++
@@ -1,9 +0,0 @@
-# Цикл while с выходом break, если условие выполнится
-while True:
-    number = int(input('Enter number'))
-    if 0 < number < 10:
-        number = number ** 2
-        print(number)
-        break
-    else:
-        print('Введено неверное значение. Введите число от 1 до 9 включительно')
